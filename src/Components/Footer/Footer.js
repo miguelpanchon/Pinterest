e9a363a5ff7e2a3dd6f42b749e5c7a7e4cdddd7f
miguelpanchon <mp607586086@gmail.com>
2024-04-24 00:39:21 +0200

    upload glitch fixings

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,41 +1,45 @@
 // Get the footer element
 const footerElement = document.getElementById("footer");
 
-// Create the first p element
-const p1 = document.createElement("p");
-p1.textContent = "by Miguel Panchón, 2024 ";
+// Create the ul element
+const ul = document.createElement("ul");
 
-// Create the second p element
-const p2 = document.createElement("p");
+// Create the first li element
+const li1 = document.createElement("li");
+li1.textContent = "by Miguel Panchón, 2024 ";
+ul.appendChild(li1);
+
+// Create the second li element
+const li2 = document.createElement("li");
 const a1 = document.createElement("a");
 a1.href = "https://github.com/miguelpanchon/Pinterest/blob/main/README.md";
 a1.textContent = "readme.md";
-p2.appendChild(a1);
+li2.appendChild(a1);
+ul.appendChild(li2);
 
-// Create the third p element
-const p3 = document.createElement("p");
+// Create the third li element
+const li3 = document.createElement("li");
 const a2 = document.createElement("a");
 a2.href = "https://github.com/miguelpanchon/Pinterest";
 a2.textContent = "this code in GitHub";
-p3.appendChild(a2);
+li3.appendChild(a2);
+ul.appendChild(li3);
 
-// Create the fourth p element
-const p4 = document.createElement("p");
+// Create the fourth li element
+const li4 = document.createElement("li");
 const a3 = document.createElement("a");
 a3.href = "https://miguelpanchon-porfolio.netlify.app/";
 a3.textContent = "My porfolio";
-p4.appendChild(a3);
+li4.appendChild(a3);
+ul.appendChild(li4);
 
-// Create the fifth p element
-const p5 = document.createElement("p");
+// Create the fifth li element
+const li5 = document.createElement("li");
 const a4 = document.createElement("a");
 a4.href = "https://www.mozilla.org/es-ES/firefox/new/";
 a4.textContent = "Optimized for Firefox";
-p5.appendChild(a4);
+li5.appendChild(a4);
+ul.appendChild(li5);
 
-// Append the p elements to the footer element
-footerElement.appendChild(p1);
-footerElement.appendChild(p2);
-footerElement.appendChild(p3);
-footerElement.appendChild(p4);
-footerElement.appendChild(p5);
+// Append the ul element to the footer element
+footerElement.appendChild(ul);
